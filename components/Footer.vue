1d1d8154a775b2bc690bd63628a16bffe26c6aca
han-tyumi <mmchamp95@gmail.com>
2021-03-10 16:48:01 -0500

    feat: add footer

---
+++ components/Footer.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+footer.flex.fixed.bottom-0.left-0.py-2.px-10.space-x-8.w-full.text-sm.text-white.bg-blue-light
+  a Email
+  a Phone
+  a FAQ
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,5 @@
 <template lang="pug">
 div
   Nuxt
+  Footer
 </template>
