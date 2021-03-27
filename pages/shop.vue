00cf5d1df4c8f71af86dee35fbddf051279956cd
han-tyumi <mmchamp95@gmail.com>
2021-03-27 00:29:22 +0000

    refactor: :art: adjust category title spacing

--- pages/shop.vue
+++ pages/shop.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
 .flex.py-8.px-10.space-x-14
-  SideBar.mt-14
+  SideBar.mt-3
 
-  .flex.flex-col.space-y-4.w-full
+  .flex.flex-col.space-y-14.w-full
     h1.text-4xl.font-semibold.capitalize {{ title }}
     NuxtChild(v-bind="{ categories }")
 </template>
